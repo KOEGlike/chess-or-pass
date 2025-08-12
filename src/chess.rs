d1bcfe2364025f24f3a6e18dce66dc23ec9ea85f
Balázs Marcell <KoegLike@proton.me>
2025-08-12 17:27:14 +0300

    trying to implement moving and moving indicators

--- src/chess.rs
+++ src/chess.rs
@@ -1,3 +1,5 @@
+use leptos::either::Either;
+use leptos::ev::select;
 use leptos::logging::*;
 use leptos::prelude::*;
 use shakmaty::*;
@@ -7,7 +9,16 @@ pub fn ChessBoard() -> impl IntoView {
     let (chess, set_chess) = signal(Chess::default());
     let current_color = move || chess.read().turn();
 
-    let x = set_chess.write().legal_moves();
+    let (selected_piece, set_selected_piece) = signal::<Option<(Square, Piece)>>(None);
+    Effect::new(move |_| {
+        chess.read();
+        set_selected_piece(None)
+    });
+
+    let move_piece = move |_| {
+        let first_legal = chess.read().legal_moves()[0];
+        set_chess.set(chess.get().play(first_legal).expect("lol no legal move"));
+    };
 
     let pieces = move || {
         chess
@@ -18,6 +29,21 @@ pub fn ChessBoard() -> impl IntoView {
                 let square = square.rotate_270();
                 let file = square.file().to_u32() + 1;
                 let rank = square.rank().to_u32() + 1;
+
+                let on_click = move |_| {
+                    log!("clicked on {square:?}");
+                    match selected_piece.get() {
+                        Some((selected_square, selected_piece)) => {
+                            if selected_square == square {
+                                set_selected_piece.set(None);
+                                return;
+                            }
+                            set_selected_piece(Some((square, piece)))
+                        }
+                        None => set_selected_piece(Some((square, piece))),
+                    }
+                };
+
                 view! {
                     <img
                         class="transition-transform duration-300 ease-in-out"
@@ -25,23 +51,73 @@ pub fn ChessBoard() -> impl IntoView {
                         style:grid-row=file.to_string()
                         class=("rotate-180", move || current_color().is_black())
                         src=piece_to_img(&piece)
+                        on:click=on_click
                     />
                 }
             })
             .collect_view()
     };
 
-    let move_piece = move |_| {
-        let first_legal = chess.read().legal_moves()[0];
-        set_chess.set(chess.get().play(first_legal).expect("lol no legal move"));
+    let move_indicators = move || {
+        log!("selected: {:?}", selected_piece.get());
+
+        let (square, piece) = match selected_piece.get() {
+            Some(s) => s,
+            None => return Either::Left(()),
+        };
+
+        log!("selected square: {square}");
+
+        let indicators = chess
+            .get()
+            .legal_moves()
+            .into_iter()
+            .filter_map(|m| {
+                log!("legal move {m:?}");
+                match m {
+                    Move::Normal { from, to, .. } | Move::EnPassant { from, to } => {
+                        if from == square {
+                            Some(to)
+                        } else {
+                            None
+                        }
+                    }
+
+                    Move::Castle { king, rook } => {
+                        if king == square {
+                            Some(rook)
+                        } else {
+                            None
+                        }
+                    }
+                    Move::Put { .. } => None,
+                }
+            })
+            .map(|s| {
+                log!("sq: {s:?}");
+                let s = s.rotate_270();
+                let file = s.file().to_u32() + 1;
+                let rank = s.rank().to_u32() + 1;
+                view! {
+                    <div
+                        class="m-4 rounded-4xl bg-amber-50"
+                        style:grid-column=rank.to_string()
+                        style:grid-row=file.to_string()
+                    />
+                }
+            })
+            .collect_view();
+
+        Either::Right(indicators)
     };
 
     view! {
         <div
-            class="bg-[url(/board.png)] w-160 h-160 grid-cols-8 grid-rows-8 grid bg-contain transition-transform duration-300 ease-in-out"
+            class="bg-[url(/board.png)] w-160 h-160 grid-cols-8 grid-rows-8 grid bg-contain transition-transform duration-300 ease-in-out rounded-md"
             class=("rotate-180", move || current_color().is_black())
         >
             {pieces}
+            {move_indicators}
         </div>
         <button on:click=move_piece>"lololol"</button>
     }
